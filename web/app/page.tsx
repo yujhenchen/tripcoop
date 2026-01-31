13256c2cba2a0a4e1588c6bd1cde215e82dbc3d6
yujhenchen <yujhen.chen93@gmail.com>
2026-01-31 18:20:25 +0800

    feat: migrate data

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,66 +1,59 @@
-// import Image from "next/image";
+import { PageContainer } from "@/components/common/pageContainer";
+import { H1 } from "@/components/typography/h1";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { NavLink } from "@/components/common/navLink";
+
+const HEADING_TEXT = "Plan Your Next Adventure";
+
+function NavButtons() {
+	// const trips = useTrips();
+
+	const items = [
+		{
+			href: `/plan/test`,
+			// trips.length > 0
+			// 	? `/plan/${trips[0].id}`
+			// 	: `/plan/${defaultTrip.id}`,
+			text: "Plan",
+		},
+		// { href: "/explore", text: "Explore" },
+	];
+	return items.map((item) => (
+		<Button
+			key={item.href}
+			variant="secondary"
+			size="lg"
+			className="rounded-full shadow-lg"
+		>
+			<NavLink href={item.href}>{item.text}</NavLink>
+		</Button>
+	));
+}
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      {/* <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-				<Image
-					className="dark:invert"
-					src="/next.svg"
-					alt="Next.js logo"
-					width={100}
-					height={20}
-					priority
+	return (
+		<PageContainer className="items-center justify-center">
+			<div className="relative">
+				<p
+					className={cn(
+						"scroll-m-20 text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-extrabold tracking-tight",
+						"absolute inset-0 pointer-events-none",
+						"text-gray-200/80 dark:text-gray-800/80",
+						"scale-105 blur-xl",
+					)}
+				>
+					{HEADING_TEXT}
+				</p>
+				<H1
+					text={HEADING_TEXT}
+					className={cn("drop-shadow-lg", "text-gray-50")}
 				/>
-				<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-					<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-						To get started, edit the page.tsx file.
-					</h1>
-					<p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-						Looking for a starting point or more instructions? Head
-						over to{" "}
-						<a
-							href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-							className="font-medium text-zinc-950 dark:text-zinc-50"
-						>
-							Templates
-						</a>{" "}
-						or the{" "}
-						<a
-							href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-							className="font-medium text-zinc-950 dark:text-zinc-50"
-						>
-							Learning
-						</a>{" "}
-						center.
-					</p>
-				</div>
-				<div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-					<a
-						className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image
-							className="dark:invert"
-							src="/vercel.svg"
-							alt="Vercel logomark"
-							width={16}
-							height={16}
-						/>
-						Deploy Now
-					</a>
-					<a
-						className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Documentation
-					</a>
-				</div>
-			</main> */}
-    </div>
-  );
+			</div>
+
+			<div className="flex gap-6 w-full place-content-center pt-12">
+				<NavButtons />
+			</div>
+		</PageContainer>
+	);
 }
---
+++ web/components/common/navLink.tsx
@@ -0,0 +1,11 @@
+import type { AnchorHTMLAttributes } from "react";
+
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+export function NavLink({ href, children, ...rest }: Props) {
+	return (
+		<a href={href} {...rest}>
+			{children}
+		</a>
+	);
+}
